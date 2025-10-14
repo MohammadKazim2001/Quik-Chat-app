a5f8f7f9e91787bec36a5b890eb330362b3be5c0
Kazim Amiri <mohammad.kazimamiri2001@gmail.com>
2025-10-14 08:35:29 +0430

    sidebar component has been conpleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500">App component</h1>
+    <div className="bg-[url('./assets/bgImage.svg')] bg-contain">
+      {/* <div className="bg-contain"> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/RightSidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RightSidebar() {
+  return (
+    <div>
+      <h2>right sidebar</h2>
+    </div>
+  );
+}
+
+export default RightSidebar;
