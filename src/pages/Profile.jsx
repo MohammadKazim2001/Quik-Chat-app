4bcbcdd8f60b88c3661c7851fa4b6f2496030c47
Kazim Amiri <mohammad.kazimamiri2001@gmail.com>
2025-10-15 14:19:30 +0430

    profile update page has been completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -83,7 +83,7 @@ function Profile() {
           </button>
         </form>
         <img
-          src={assets.logo_icon}
+          src={authUser?.profilePic || assets.logo_icon}
           alt=""
           className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${
             selectedImg && "rounded-full"
